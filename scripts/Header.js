f26e6e383da40b5a6ec26233900c557decfc0593
Arseniy Komar <arsenjik12@gmail.com>
2025-10-05 17:18:17 +0300

    Done: html for header, css for header (with adaptive), js for header. Header full done

---
+++ scripts/Header.js
@@ -0,0 +1,90 @@
+class Header {
+    selectors = {
+        root: '[data-js-header]',
+        overlay: '[data-js-header-overlay]',
+        openBurgerMenuButton: '[data-js-open-header-burger-menu-button]',
+        closeBurgerMenuButton: '[data-js-close-header-burger-menu-button]',
+    }
+
+    stateClasses = {
+        isActive: 'is-active',
+        isLock: 'is-lock',
+    }
+
+    constructor() {
+        this.rootElement = document.querySelector(this.selectors.root);
+        this.overlayElement = document.querySelector(this.selectors.overlay);
+        this.openBurgerMenuButtonElement = this.rootElement?.querySelector(this.selectors.openBurgerMenuButton);
+        this.closeBurgerMenuButtonElement = document.querySelector(this.selectors.closeBurgerMenuButton);
+        this.resizeTimer = null;
+
+        this.bindEvents();
+    }
+
+    positionOverlayMenu() {
+        const rect = this.openBurgerMenuButtonElement.getBoundingClientRect();
+        const scrollX = document.documentElement.scrollLeft;
+        const scrollY = document.documentElement.scrollTop;
+
+        // Позиционируем по правому нижнему углу кнопки
+
+        this.overlayElement.style.left = `${rect.right- this.overlayElement.getBoundingClientRect().width  + scrollX}px`;
+        this.overlayElement.style.top = `${rect.top + 30 + scrollY}px`;
+    }
+
+    openBurgerMenu() {
+        this.openBurgerMenuButtonElement.classList.toggle(this.stateClasses.isActive);
+        this.overlayElement.classList.toggle(this.stateClasses.isActive);
+        document.documentElement.classList.toggle(this.stateClasses.isLock);
+    }
+
+    onOpenBurgerButtonClick = () => {
+        if (!this.openBurgerMenuButtonElement || !this.closeBurgerMenuButtonElement || !this.overlayElement)
+            return;
+
+        this.positionOverlayMenu();
+        this.openBurgerMenu();
+    }
+
+    closeBurgerMenu() {
+        this.closeBurgerMenuButtonElement.classList.remove(this.stateClasses.isActive);
+        this.openBurgerMenuButtonElement.classList.remove(this.stateClasses.isActive);
+        this.overlayElement.classList.remove(this.stateClasses.isActive);
+        document.documentElement.classList.remove(this.stateClasses.isLock);
+    }
+
+    onCloseBurgerButtonClick = () => {
+        this.closeBurgerMenu();
+    }
+
+    onResize = () => {
+        clearTimeout(this.resizeTimer);
+
+        this.resizeTimer = setTimeout(() => {
+            this.positionOverlayMenu();
+
+            if (window.innerWidth >= 768) {
+                this.closeBurgerMenu();
+            }
+        }, 300)
+    }
+
+    onDocumentClick = () => {
+        const isClickInsideOverlay = this.overlayElement?.contains(event.target);
+        const isClickOnBurgerButton = this.openBurgerMenuButtonElement?.contains(event.target);
+
+        // Если клик был снаружи и меню открыто - закрываем
+        if (!isClickInsideOverlay && !isClickOnBurgerButton) {
+            this.closeBurgerMenu();
+        }
+    }
+
+    bindEvents() {
+        this.openBurgerMenuButtonElement?.addEventListener('click', this.onOpenBurgerButtonClick);
+        this.closeBurgerMenuButtonElement?.addEventListener('click', this.onCloseBurgerButtonClick);
+        window.addEventListener('resize', this.onResize);
+        document.addEventListener('click', this.onDocumentClick);
+    }
+}
+
+export default Header;
